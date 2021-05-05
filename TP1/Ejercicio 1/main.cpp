2cfb4f01aca40d416942d6fcdd41103446452dbf
Nick-bit-dev <83304251+Nick-bit-dev@users.noreply.github.com>
2021-05-05 19:21:27 -0300

    Add files via upload

---
+++ TP1/Ejercicio 1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float x,y;
+    cout << "Ingrese su velocidad en m/s" << endl;
+    cin >> x;
+    y = x * 3.6;
+    cout << "Su velocidad en k/h es de " << y << endl;
+    return 0;
+}
